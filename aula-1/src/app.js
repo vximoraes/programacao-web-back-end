766e44922ec597e4dc55ac66d60c58e3b8610ebf
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-01 22:26:23 -0400

    Export app instance and add routes for grupos, unidades, and usuarios

--- aula-1/src/app.js
+++ aula-1/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const app = express()
+export const app = express()
 
 const grupos = [
     {id: 1, 'admin': 'Administradores'},
@@ -15,3 +15,26 @@ const unidades = {
         3: 'Guajará'
     }
 }
+
+const usuarios = [
+    {id: 1, 'vinimoraes': 'Vinícius Moraes'},
+    {id: 2, 'tartinhas': 'Eduardo Tartas'},
+    {id: 3, 'yurizin': 'Yuri Zetoles'},
+    {id: 4, 'arthuzin': 'Arthur Gomes'}
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('Bem-vindo ao auth!')
+})
+
+app.get('/grupos', (req, res) => {
+    res.status(200).json(grupos)
+})
+
+app.get('/unidades', (req, res) => {
+    res.status(200).json(unidades)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(usuarios)
+})
