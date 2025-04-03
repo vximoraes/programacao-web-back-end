2b6940c4f7b772f151b5e1a60809544c207f9e39
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-03 19:12:25 -0400

    Refactor server.js to use app instance and remove unused routing code

--- aula-1/server.js
+++ aula-1/server.js
@@ -1,21 +1,22 @@
-import http from "http"
+import app from "./src/app.js"
+// import http from "http"
 
 const port = 3001
 
-const rotas = {
-    '/': 'Bem-vindo ao Auth!',
-    '/usuarios': 'Lista de usuarios',
-    '/grupos': 'Lista de grupos',
-    '/programas': 'Lista de programas',
-    '/unidades': 'Lista de unidades',
-    '/usuarios/1': 'Detalhes do usuario 1',
-}
+// const rotas = {
+//     '/': 'Bem-vindo ao Auth!',
+//     '/usuarios': 'Lista de usuarios',
+//     '/grupos': 'Lista de grupos',
+//     '/programas': 'Lista de programas',
+//     '/unidades': 'Lista de unidades',
+//     '/usuarios/1': 'Detalhes do usuario 1',
+// }
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'text/plan'})
-    res.end(rotas[req.url])
-})
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type':'text/plan'})
+//     res.end(rotas[req.url])
+// })
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Servidor escutando porta http://localhost:${port}`)
 })
