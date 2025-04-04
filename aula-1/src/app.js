4c060519149ef0f23570708596a3e5be5039e133
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-03 21:13:22 -0400

    Update grupos and usuarios data structure; enhance /usuarios endpoint for name-based lookup

--- aula-1/src/app.js
+++ aula-1/src/app.js
@@ -3,24 +3,25 @@ import express from 'express'
 export const app = express()
 
 const grupos = [
-    {id: 1, 'admin': 'Administradores'},
-    {id: 2, 'gerentes': 'Gerentes'},
-    {id: 3, 'usuarios': 'Usuários limitados'}
+    {id: 1, nome: 'Todos'},
+    {id: 2, nome: 'Administradores'},
+    {id: 3, nome: 'Gerentes'},
+    {id: 4, nome: 'Usuários limitados'}
 ]
 
-const unidades = {
+const unidades = [{
     'unidades': {
         1: 'Vilhena',
         2: 'Cacoal',
         3: 'Guajará'
     }
-}
+}]
 
 const usuarios = [
-    {id: 1, 'vinimoraes': 'Vinícius Moraes'},
-    {id: 2, 'tartinhas': 'Eduardo Tartas'},
-    {id: 3, 'yurizin': 'Yuri Zetoles'},
-    {id: 4, 'arthuzin': 'Arthur Gomes'}
+    {id: 1, nome: 'Vinícius'},
+    {id: 2, nome: 'Eduardo Tartas'},
+    {id: 3, nome: 'Yuri Zetoles'},
+    {id: 4, nome: 'Arthur Gomes'}
 ]
 
 app.get('/', (req, res) => {
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/grupos', (req, res) => {
+    console.log(req.query)
     res.status(200).json(grupos)
 })
 
@@ -36,7 +38,18 @@ app.get('/unidades', (req, res) => {
 })
 
 app.get('/usuarios', (req, res) => {
-    res.status(200).json(usuarios)
+    console.log(req.query)
+
+    const requisicao = req.query.nome
+    
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].nome == requisicao) {
+            console.log(usuarios[i].nome)
+            res.status(200).json([usuarios[i].id, usuarios[i].nome])
+        }
+    }
+        
+    res.status(404).send(`Nenhum usuário encontrado com o nome ${requisicao}.`)
 })
 
 export default app
