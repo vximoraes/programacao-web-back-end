75cb561a1e043af627cd14888ca697279dfc49d3
Vinícius Moraes <viniciusmoraesvha@gmail.com>
2025-04-09 21:00:10 -0400

    Remove commented-out route definitions and server creation logic from server.js

--- aula-1/server.js
+++ aula-1/server.js
@@ -6,20 +6,6 @@ dotenv.config()
 
 const port = process.env.PORT
 
-// const rotas = {
-//     '/': 'Bem-vindo ao Auth!',
-//     '/usuarios': 'Lista de usuarios',
-//     '/grupos': 'Lista de grupos',
-//     '/programas': 'Lista de programas',
-//     '/unidades': 'Lista de unidades',
-//     '/usuarios/1': 'Detalhes do usuario 1',
-// }
-
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {'Content-Type':'text/plan'})
-//     res.end(rotas[req.url])
-// })
-
 app.listen(port, () => {
     console.log(`Servidor escutando porta http://localhost:${port}`)
 })
